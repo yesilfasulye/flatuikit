bc991f40315a7aefe1a48e5246679e82a3fa9f6c
Burak Kaya <brkkaya@gmail.com>
2014-08-20 10:49:24 +0300

    Delete download.php

--- download.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$hit_count = @file_get_contents('count.txt');
-	$hit_count++;
-	@file_put_contents('count.txt', $hit_count);
-
-	header('Location: http://www.burakkaya.com/lab/flat/Flat_UI_Kit.zip');
-
-?>
